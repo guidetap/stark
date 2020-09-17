37e6dd1fa60b68bbc41bd6040126b893002cab39
sausageRoll <drozdovigori1996@gmail.com>
2020-09-17 14:34:55 +0200

    Add oauth0 client and server header check

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,16 +16,23 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
+
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+
+    implementation ("org.springframework.boot:spring-boot-starter-security")
+    implementation ("org.springframework.security:spring-security-oauth2-resource-server")
+    implementation ("org.springframework.security:spring-security-oauth2-jose")
+    implementation ("org.springframework.security:spring-security-oauth2-client")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
     }
+
     testImplementation("io.projectreactor:reactor-test")
 }
 
