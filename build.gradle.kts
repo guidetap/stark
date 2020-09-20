3a8179ff6b3bb1f22124eed6140db3a8ffaa2bd7
sausageRoll <drozdovigor1996@gmail.com>
2020-09-20 11:54:11 +0200

    Add Mongo connection

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,6 +39,8 @@ dependencies {
     implementation ("io.ktor:ktor-client-cio:$ktorVersion")
     implementation ("io.ktor:ktor-client-logging-jvm:$ktorVersion")
 
+    implementation("org.litote.kmongo:kmongo-coroutine:4.1.2")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
     }
---
+++ src/main/kotlin/com/guidetap/stark/config/MongoConnectionConfig.kt
@@ -0,0 +1,18 @@
+package com.guidetap.stark.config
+
+import com.mongodb.reactivestreams.client.MongoClient
+import org.litote.kmongo.coroutine.CoroutineDatabase
+import org.litote.kmongo.coroutine.coroutine
+import org.litote.kmongo.reactivestreams.withKMongo
+import org.springframework.boot.autoconfigure.mongo.MongoProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class MongoConnectionConfig {
+
+  @Bean
+  fun mongoDatabase(mongoClient: MongoClient, mongoProperties: MongoProperties): CoroutineDatabase =
+      mongoClient.getDatabase(mongoProperties.database).withKMongo().coroutine
+
+}
