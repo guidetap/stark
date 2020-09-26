2fddd25281672d38a9ed544e8b5abd3dca9cc12e
sausageRoll <drozdovigor1996@gmail.com>
2020-09-26 17:15:35 +0200

    Fix tests

--- src/test/kotlin/com/guidetap/stark/AbstractIntegrationTest.kt
+++ src/test/kotlin/com/guidetap/stark/AbstractIntegrationTest.kt
@@ -3,30 +3,40 @@ package com.guidetap.stark
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.testcontainers.containers.DockerComposeContainer
 import org.testcontainers.containers.wait.strategy.Wait
 import java.io.File
 
+
 @ExtendWith(SpringExtension::class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 class AbstractIntegrationTest {
 
-  private val container: KDockerComposeContainer =
-    KDockerComposeContainer(
-      File("src/test/resources/compose-test.yml")
-    )
-      .withExposedService("mongo", 27017, Wait.forListeningPort())
+  companion object {
+    private val container: KDockerComposeContainer =
+      KDockerComposeContainer(
+        File("src/test/resources/compose-test.yml")
+      )
+        .withLocalCompose(true)
+        .withExposedService("mongo", 27017, Wait.forListeningPort())
 
-  init {
-    container.start()
-    System.setProperty(
-      "spring.data.mongodb.port",
-      container.getServicePort("mongo", 27017).toString()
-    )
+    init {
+      container.start()
+    }
+
+    @JvmStatic
+    @DynamicPropertySource
+    private fun mongoDBProperties(registry: DynamicPropertyRegistry) {
+      registry.add("spring.data.mongodb.port") {
+        container.getServicePort("mongo", 27017).toString()
+      }
+    }
+
   }
-
 }
 
 class KDockerComposeContainer(file: File) : DockerComposeContainer<KDockerComposeContainer>(file)
