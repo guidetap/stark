d04756a847227a87d8422f006a44f80bed665161
sausageRoll <drozdovigor1996@gmail.com>
2020-10-14 15:02:00 +0200

    Fix pattern and issue with not storing last batch

--- src/main/kotlin/com/guidetap/stark/service/ShopifyCustomerService.kt
+++ src/main/kotlin/com/guidetap/stark/service/ShopifyCustomerService.kt
@@ -19,7 +19,7 @@ class ShopifyCustomerService(
 ) {
 
   companion object {
-    val SHOPIFY_FORMATTER: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm")
+    val SHOPIFY_FORMATTER: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:SS")
   }
 
   private val log = LoggerFactory.getLogger(javaClass)
@@ -48,13 +48,14 @@ class ShopifyCustomerService(
       while (true) {
         log.info("process='getAllCustomers' message='new request prepared' userId='$userId' pageInfo='$pageInfo'")
         val customerSince = getCustomers(userId, pageInfo, lastSyncDate)
+        customerSince.customerResponse.customers.forEach {
+          emit(it)
+        }
         if (customerSince.pageInfoUrl == null) {
           break
         }
         pageInfo = urlParser.extractParamUrl(customerSince.pageInfoUrl, "page_info")
-        customerSince.customerResponse.customers.forEach {
-          emit(it)
-        }
+
       }
     }
 }
